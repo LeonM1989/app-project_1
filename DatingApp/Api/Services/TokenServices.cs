c39f7a94df50edcfa0285cb35a9ceb1011720c2d
Leon M <leon5916619@gmail.com>
2022-08-28 21:12:26 +0300

    Create JWT_Token

---
+++ DatingApp/Api/Services/TokenServices.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Entities;
+using Api.interfaces;
+
+namespace Api.Services
+{
+    public class TokenService: ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
