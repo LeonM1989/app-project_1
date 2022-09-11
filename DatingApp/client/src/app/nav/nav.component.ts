172ee2f46f5b6f863273cc442ce505e88e125d1c
Leon M <leon5916619@gmail.com>
2022-09-11 20:17:15 +0300

    routing_in_code_html

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../services/account.service';
 import { User } from '../models/User';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +14,8 @@ export class NavComponent implements OnInit {
   model: any = {};
   currentUser$?: Observable<User>;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, 
+    private router: Router) {
     this.currentUser$ = this.accountService.currentUser$;
   }
 
@@ -25,6 +27,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model) // returning observable (it's lazy)
       .subscribe({
         next: response => {
+          this.router.navigateByUrl('/members');
           console.log(response);
         },
         error: error => {
@@ -36,6 +39,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
