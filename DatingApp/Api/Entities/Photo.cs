8e8441b55e0c08fafd6854feeb1cd0182bcd4c6f
Leon M <leon5916619@gmail.com>
2022-09-28 18:52:54 +0300

    generating seed data

--- DatingApp/Api/Entities/Photo.cs
+++ DatingApp/Api/Entities/Photo.cs
@@ -15,7 +15,7 @@ namespace Api.Entities
         public string PublicId {get; set;}
         
 
-        // this is 'fully definding' relationship
+        // this is 'fully definding' relationship//
         public AppUser AppUser {get; set;}
         public int AppUserId {get; set;}
     }
