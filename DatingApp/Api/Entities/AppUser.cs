0b16819667872b625782790f11cef3be8f20a7c7
Leon M <leon5916619@gmail.com>
2022-09-28 18:00:48 +0300

    Extending the user entity

--- DatingApp/Api/Entities/AppUser.cs
+++ DatingApp/Api/Entities/AppUser.cs
@@ -12,5 +12,27 @@ namespace Api.Entities
 
         public byte[] PasswordHash{get; set;}
         public byte[] PasswordSalt{get; set;}
+
+
+
+
+          public DateTime DateOfBirth{get; set;}
+          public string KnownAs {get; set;}
+          public  DateTime created {get; set;} = DateTime.Now; // initial value
+
+          public  DateTime LastActive {get; set;}
+
+
+
+          public string Gender {get; set;}
+
+          public string Introduction {get; set;}
+          public string LokingFor {get; set;}
+          public string Interests {get; set;}
+
+          public string City {get; set;}
+          public string Country {get; set;}
+
+          public ICollection<Photo> Photos{get; set;}
     }
 }
