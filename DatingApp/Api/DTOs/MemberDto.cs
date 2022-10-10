4388fb3a9c42897458ade970ca89c8c074862b78
Leon M <leon5916619@gmail.com>
2022-10-10 13:58:11 +0300

    Using AutoMapper

--- DatingApp/Api/DTOs/MemberDto.cs
+++ DatingApp/Api/DTOs/MemberDto.cs
@@ -8,8 +8,8 @@ namespace Api.DTOs
     public class MemberDto
     {
          public int Id {get; set;}
-        public string UserName {get; set;}
-
+         public string UserName {get; set;}
+          
 
           public int Age {get; set;}
           public string KnownAs {get; set;}
