ffe48a8eaaafe7224d330c821cec61fa08335c27
Leon M <leon5916619@gmail.com>
2022-10-10 14:10:39 +0300

    Configuration AutoMapper

--- DatingApp/Api/DTOs/MemberDto.cs
+++ DatingApp/Api/DTOs/MemberDto.cs
@@ -10,7 +10,7 @@ namespace Api.DTOs
          public int Id {get; set;}
          public string UserName {get; set;}
           
-
+          public string PhotoUrl{get; set;}
           public int Age {get; set;}
           public string KnownAs {get; set;}
           public  DateTime created {get; set;} 
--- DatingApp/Api/Helpers/AutoMapperProfiles.cs
+++ DatingApp/Api/Helpers/AutoMapperProfiles.cs
@@ -13,7 +13,13 @@ namespace Api.Helpers
        public AutoMapperProfiles()
         {
             //we want to map AppUser ==> MemberDto
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(
+                dest => dest.PhotoUrl,
+                opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url);
+                }
+            );
 
             //we want to mao Photo ==> PhotoDto
             CreateMap<Photo, PhotoDto>();
